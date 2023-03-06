c6ca5de21526667f6f456777e3cdb02f6c59b866
RBO <raphael.bobet2022@campus-eni.fr>
2023-03-06 15:13:55 +0100

    creation of Outing (entity and repo)

---
+++ src/Entity/Outing.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OutingRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OutingRepository::class)]
+class Outing
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateTimeStart = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registrationLimitDate = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nbParticipantsMax = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $overview = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDateTimeStart(): ?\DateTimeInterface
+    {
+        return $this->dateTimeStart;
+    }
+
+    public function setDateTimeStart(\DateTimeInterface $dateTimeStart): self
+    {
+        $this->dateTimeStart = $dateTimeStart;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationLimitDate(): ?\DateTimeInterface
+    {
+        return $this->registrationLimitDate;
+    }
+
+    public function setRegistrationLimitDate(\DateTimeInterface $registrationLimitDate): self
+    {
+        $this->registrationLimitDate = $registrationLimitDate;
+
+        return $this;
+    }
+
+    public function getNbParticipantsMax(): ?int
+    {
+        return $this->nbParticipantsMax;
+    }
+
+    public function setNbParticipantsMax(int $nbParticipantsMax): self
+    {
+        $this->nbParticipantsMax = $nbParticipantsMax;
+
+        return $this;
+    }
+
+    public function getOverview(): ?string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): self
+    {
+        $this->overview = $overview;
+
+        return $this;
+    }
+}
