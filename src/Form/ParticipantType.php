298faf796ea18dedceb93a6d477a4d29ee25f203
mmachlaf2022 <manar.machlaf2022@campus-eni.fr>
2023-03-07 16:36:22 +0100

    En cours formulaire mon profil

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -2,28 +2,76 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\Mapping\Entity;
 use phpDocumentor\Reflection\PseudoType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('lastName')
-            ->add('firstName')
-            ->add('phone')
-            ->add('isEnabled')
-            ->add('photo')
-            ->add('outings')
-            ->add('campus')
-            ->add('pseudo')
+            ->add('pseudo',TextType::class,[
+                'label'=> 'Pseudo :'
+            ])
+
+            ->add('firstName',TextType::class,[
+                'label' => 'Prénom :'])
+
+            ->add('lastName', TextType::class,[
+                'label'=>'Nom :'
+            ])
+
+            ->add('phone',TextType::class,[
+                'label'=>'Téléphone'
+            ])
+
+            ->add('email', EmailType::class,[
+            'label'=>'Email'])
+
+            ->add('plainPassword', RepeatedType::class, [
+
+                'type'=>PasswordType::class,
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => [
+                    'label' => 'Mot de passe',
+            ],
+                'second_options' => [
+                    'label' => 'Confirmation',
+                    ],
+                'invalid_message' => 'The password fields must match.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+            ])
+
+            ->add('campus',EntityType::class, [
+                'class'=>Campus::class,
+                'choice_label'=> 'name',
+                'label'=>'Campus',
+                'query_builder'=> function(CampusRepository $campusRepository){
+                $qb = $campusRepository->createQueryBuilder("campus");
+                $qb->addOrderBy("campus.name");
+                return $qb;
+                }
+            ])
+
+
+
+            ->add('photo',FileType::class,['label'=>'Ma photo'])
         ;
     }
 
